7d62c1eec874a2d794ef25d91422657ee0523b4a
Collin Newman <collin.newman37@gmail.com>
2021-01-14 11:47:53 -0500

    Removed unneeded code from server/index.js

--- database/Reviews.js
+++ database/Reviews.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line no-unused-vars
 const db = require('./index.js');
 
 const reviewSchema = new mongoose.Schema({
--- server/index.js
+++ server/index.js
@@ -2,20 +2,6 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-const axios = require('axios');
-const aws = require('../database/imagesBucketConfig.js');
-
-app.get('/images', (req, res) => {
-  const authString = `AWS ${aws.AWSAccessKeyId}:${aws.AWSSecretKey}`;
-  axios.get(aws.endpoint, {
-    headers: {
-      Date: (new Date()).toUTCString(),
-      Authorization: authString,
-    },
-  })
-    .then((response) => res.send(response))
-    .catch((err) => res.send(err));
-});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
