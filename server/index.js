28d5bd7fbc3d21f469dff3abefd595fa0a0693c6
Collin Newman <collin.newman37@gmail.com>
2021-01-14 19:38:42 -0500

    Added initial routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(parser());
 
+app.get('/api/reviews', (req, res) => {
+
+});
+
+app.post('/api/reviews', (req, res) => {
+
+});
+
+app.patch('/api/reviews/:id', (req, res) => {
+
+});
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port', port);
