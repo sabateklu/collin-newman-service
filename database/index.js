9c18c6549db36d3ea59886d99638bf931469e457
Collin Newman <collin.newman37@gmail.com>
2021-01-14 21:27:45 -0500

    Added { useUnifiedTopology: true } to mongodb connection.

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true });
+const db = mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true }, { useUnifiedTopology: true });
 module.exports = db;
