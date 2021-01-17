8d6c088dadafa0e8e57e2cf914ae63f761b41901
Collin Newman <collin.newman37@gmail.com>
2021-01-16 20:22:57 -0500

    Changed where the profile pics are fetched from. Previously they were being fetched from photots not meant to be a thumbnail.

--- dev/database/seed.js
+++ dev/database/seed.js
@@ -3,49 +3,55 @@
 const Faker = require('faker');
 const Reviews = require('../../database/Reviews.js');
 const mongoose = require('mongoose');
+const fetch = require('node-fetch');
 
 const generateData = () => {
-  Reviews.remove((err) => {
-    if (err) {
-      console.log(Err);
-    } else {
-      console.log('Database dropped');
-    }
-  });
-  const dummyData = [];
-  const destinations = ['Phitsanulok', 'Bangkok', 'Phuket', 'Trang', 'Ayutthaya'];
+  fetch('https://randomuser.me/api/?results=5000')
+    .then((response) => response.json())
+    .then((fakeUsers) => {
+      Reviews.remove((err) => {
+        if (err) {
+          console.log(Err);
+        } else {
+          console.log('Database dropped');
+        }
+      });
+      const dummyData = [];
+      const destinations = ['Phitsanulok', 'Bangkok', 'Phuket', 'Trang', 'Ayutthaya'];
 
-  for (let j = 0; j < destinations.length; j++) {
-    for (let i = 0; i < 100; i += 1) {
-      const randomImages = [];
-      for (let k = 0; k < Math.floor(Math.random() * 6); k++) {
-        randomImages.push(`http://d20lp9tw1uk7y6.cloudfront.net/images/tripadvisor_thailand_${Math.floor(Math.random() * 100)}.jpg`)
+      for (let j = 0; j < destinations.length; j++) {
+        for (let i = 0; i < 100; i += 1) {
+          const randomImages = [];
+          for (let k = 0; k < Math.floor(Math.random() * 6); k++) {
+            randomImages.push(`http://d20lp9tw1uk7y6.cloudfront.net/images/tripadvisor_thailand_${Math.floor(Math.random() * 100)}.jpg`)
+          }
+          dummyReview = {
+            userName: `${Faker.name.firstName()} ${Faker.name.lastName()}`,
+            profilePic: `${fakeUsers.results[Math.floor(Math.random() * 5000)].picture.thumbnail}`,
+            created_at: Faker.date.past(),
+            userHomeLocation: Faker.address.country(),
+            images: randomImages,
+            starRating: Math.floor(Math.random() * (5 - 1) + 1),
+            reviewTitle: Faker.lorem.words(),
+            reviewBody: Faker.lorem.paragraph(),
+            dateOfExperience: Faker.date.past(),
+            helpfulVotes: Math.floor(Math.random() * (20 - 1) + 1),
+            destination: destinations[j]
+          };
+          dummyData.push(dummyReview);
+        }
       }
-      dummyReview = {
-        userName: `${Faker.name.firstName()} ${Faker.name.lastName()}`,
-        profilePic: `http://d20lp9tw1uk7y6.cloudfront.net/profilePics/profilePic_people_${i}.jpg`,
-        created_at: Faker.date.past(),
-        userHomeLocation: Faker.address.country(),
-        images: randomImages,
-        starRating: Math.floor(Math.random() * (5 - 1) + 1),
-        reviewTitle: Faker.lorem.words(),
-        reviewBody: Faker.lorem.paragraph(),
-        dateOfExperience: Faker.date.past(),
-        helpfulVotes: Math.floor(Math.random() * (20 - 1) + 1),
-        destination: destinations[j]
-      };
-      dummyData.push(dummyReview);
-    }
-  }
 
-  Reviews.create(dummyData, (err) => {
-    if (err) {
-      console.log('Err', err);
-    } else {
-      console.log('Seeding complete');
-      mongoose.disconnect();
-    }
-  });
+      Reviews.create(dummyData, (err) => {
+        if (err) {
+          console.log('Err', err);
+        } else {
+          console.log('Seeding complete');
+          mongoose.disconnect();
+        }
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 generateData();
